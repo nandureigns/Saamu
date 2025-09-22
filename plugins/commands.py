033f9020a12da92a3f382d4f85c16d8e1086fd1b
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-09-22 09:12:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -281,9 +281,9 @@ async def start(client, message):
                         return
                 await msg.edit_caption(f_caption)
                 return
-            except:
-                pass
-                return await message.reply('No such file exist.')
+        except:
+            pass
+            return await message.reply('No such file exist.')
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
